148e1aec8e093790c3061ea887cb2487e3b8bfc8
Arkadiy Radchenko <a-ti-radeon@yandex.ru>
2025-12-03 23:01:39 +0300

    add tests for fenwick tree 2d

--- tests/unittests/sem2/fenwick_tree/test_2d.cc
+++ tests/unittests/sem2/fenwick_tree/test_2d.cc
@@ -6,42 +6,6 @@
 
 using namespace std;
 
-TEST(FenwickTree2D, TestSize1x1) {
-    FenwickTree<int> tree(1, 1);
-
-    EXPECT_EQ(tree.Sum(0, 0, 0, 0), 0);
-
-    tree.Add(0, 0, 544);
-    EXPECT_EQ(tree.Sum(0, 0, 0, 0), 544);
-
-    tree.Add(0, 0, -544);
-    EXPECT_EQ(tree.Sum(0, 0, 0, 0), 0);
-}
-
-TEST(FenwickTree2D, TestSize1x2) {
-    FenwickTree<int> tree(1, 2);
-
-    EXPECT_EQ(tree.Sum(0, 0, 0, 1), 0);
-
-    tree.Add(0, 0, 544);
-    tree.Add(0, 1, 501);
-    EXPECT_EQ(tree.Sum(0, 0, 0, 0), 544);
-    EXPECT_EQ(tree.Sum(0, 1, 0, 1), 501);
-    EXPECT_EQ(tree.Sum(0, 0, 0, 1), 501 + 544);
-}
-
-TEST(FenwickTree2D, TestSize2x1) {
-    FenwickTree<int> tree(2, 1);
-
-    EXPECT_EQ(tree.Sum(0, 0, 1, 0), 0);
-
-    tree.Add(0, 0, 544);
-    tree.Add(1, 0, 501);
-    EXPECT_EQ(tree.Sum(0, 0, 0, 0), 544);
-    EXPECT_EQ(tree.Sum(1, 0, 1, 0), 501);
-    EXPECT_EQ(tree.Sum(0, 0, 1, 0), 501 + 544);
-}
-
 struct GetQuery {
     int x0, y0, x1, y1;
 };
@@ -55,7 +19,7 @@ struct NaiveFenwickTree {
     int n, m;
     vector<vector<T>> t;
 
-    NaiveFenwickTree(int n, int m) : n(n), m(m), t(n + 1, vector<T>(m + 1)) {}
+    NaiveFenwickTree(int n, int m) : n(n), m(m), t(n, vector<T>(m)) {}
 
     void Add(int x, int y, int v) {
         t[x][y] += v;
@@ -82,9 +46,9 @@ vector<variant<GetQuery, AddQuery>> generate_queries(int n, int m, int q, defaul
             int y0 = y_rand(eng);
             int y1 = y_rand(eng);
 
-            if (x1 > x0)
+            if (x1 < x0)
                 swap(x1, x0);
-            if (y1 > y0)
+            if (y1 < y0)
                 swap(y1, y0);
             queries.push_back(std::move(GetQuery { x0, y0, x1, y1 }));
         } else {
@@ -93,6 +57,8 @@ vector<variant<GetQuery, AddQuery>> generate_queries(int n, int m, int q, defaul
             queries.push_back(std::move(AddQuery { x, y, add_value(eng) }));
         }
     }
+
+    return queries;
 }
 
 bool random_test_compare_with_naive(int n, int m, int q, int seed) {
@@ -104,7 +70,7 @@ bool random_test_compare_with_naive(int n, int m, int q, int seed) {
     for (const auto& query : queries) {
         if (query.index() == 0) {
             auto [x0, y0, x1, y1] = get<GetQuery>(query);
-            if (tree.Sum(x0, y0, x1, y1) == naive_tree.Sum(x0, y0, x1, y1))
+            if (tree.Sum(x0, y0, x1, y1) != naive_tree.Sum(x0, y0, x1, y1))
                 return false;
         } else {
             auto [x, y, v] = get<AddQuery>(query);
@@ -116,15 +82,39 @@ bool random_test_compare_with_naive(int n, int m, int q, int seed) {
     return true;
 }
 
-TEST(FenwickTree2D, TestBig1) {
-    ASSERT_TRUE(random_test_compare_with_naive(1000, 1000, 200, 82736478));
+TEST(FenwickTree2D, TestRandomSmall1) {
+    ASSERT_TRUE(random_test_compare_with_naive(1, 1, 1, 1337));
 }
 
-TEST(FenwickTree2D, TestBig2) {
-    ASSERT_TRUE(random_test_compare_with_naive(500, 500, 400, 42343223));
+TEST(FenwickTree2D, TestRandomSmall2) {
+    ASSERT_TRUE(random_test_compare_with_naive(1, 2, 3, 236634));
 }
 
-TEST(FenwickTree2D, TestBig3) {
-    ASSERT_TRUE(random_test_compare_with_naive(500, 500, 400, 42343223));
+TEST(FenwickTree2D, TestRandomSmall3) {
+    ASSERT_TRUE(random_test_compare_with_naive(2, 1, 5, 3543234));
+}
+
+TEST(FenwickTree2D, TestRandomMedium1) {
+    ASSERT_TRUE(random_test_compare_with_naive(50, 30, 5000, 543534));
+}
+
+TEST(FenwickTree2D, TestRandomMedium2) {
+    ASSERT_TRUE(random_test_compare_with_naive(80, 40, 5000, 42342342));
+}
+
+TEST(FenwickTree2D, TestRandomMedium3) {
+    ASSERT_TRUE(random_test_compare_with_naive(70, 70, 5000, 3543234));
+}
+
+TEST(FenwickTree2D, TestRandomBig1) {
+    ASSERT_TRUE(random_test_compare_with_naive(1000, 1000, 300, 82736478));
+}
+
+TEST(FenwickTree2D, TestRandomBig2) {
+    ASSERT_TRUE(random_test_compare_with_naive(500, 500, 1000, 42343223));
+}
+
+TEST(FenwickTree2D, TestRandomBig3) {
+    ASSERT_TRUE(random_test_compare_with_naive(1000, 1000, 300, 92399238));
 }
 
